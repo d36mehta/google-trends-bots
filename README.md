09f3187c8d57ba2e748052e7122cd40db0e7c07d
drastimehta <drastiimehtaa@gmail.com>
2021-09-18 00:24:32 -0400

    inital commit (organize bots into folders based on country)

---
+++ README.md
@@ -0,0 +1,11 @@
+# Google Trends Twitter bots
+
+Google Trends Twitter bots to tweet the top three trending searches for multiple countries (so far only Canada) every six hours.
+
+Uses AWS Lambda, AWS CloudWatch, and Serverless.
+
+Current twitter bots:
+
+- [Canada](https://twitter.com/GoogleTrends_CA)
+
+![alt text](https://pbs.twimg.com/profile_images/880534343008927744/cRCD7iF7_400x400.jpg)
---
+++ google-trends-bot-us/handler.js
@@ -0,0 +1,52 @@
+"use strict";
+const axios = require("axios");
+const OAuth2 = require("oauth").OAuth2;
+const Twit = require("twit");
+
+// USA
+module.exports.botus = async () => {
+  try {
+    const tw = new Twit({
+      consumer_key: process.env.CONSUMER_KEY,
+      consumer_secret: process.env.CONSUMER_SECRET,
+      access_token: process.env.ACCESS_TOKEN_KEY,
+      access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+    });
+
+    const response = await axios({
+      url:
+        "https://trends.google.com/trends/api/dailytrends?hl=en-US&tz=240&geo=US&ns=15",
+    });
+    let responseFormatted = response.data.substring(5);
+    const res = JSON.parse(responseFormatted);
+    // create 3 tweets
+    let tweets = [];
+    for (let i = 0; i < 3; i++) {
+      tweets.push(
+        `${i + 1}. ${
+          res.default.trendingSearchesDays[0].trendingSearches[i].title.query
+        } - ${
+          res.default.trendingSearchesDays[0].trendingSearches[i]
+            .formattedTraffic
+        } searches! 🕵️ 🇺🇸 \n\n 📰 Related: ${
+          res.default.trendingSearchesDays[0].trendingSearches[i].articles[0]
+            .url
+        }`
+      );
+    }
+    // post the top 3 in reverse order
+    for (let i = 2; i >= 0; i--) {
+      console.log("Currently trying to tweet this: " + tweets[i]);
+      try {
+        await tw.post("statuses/update", { status: tweets[i] });
+        console.log("Tweet posted! 🎉");
+      } catch (err) {
+        console.log("An error has occurred while trying to post 😪");
+        console.log(err);
+      }
+    }
+  } catch (err) {
+    console.log("An error has occurred 🥲");
+    console.log(err);
+  }
+};
